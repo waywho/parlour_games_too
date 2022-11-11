1ce968ee345d65eda6d9588356504a498bccf6cc
WeiHsi Hu <walzerfan@yahoo.com>
2022-11-11 21:49:32 +0100

    Add new user and session management

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_224158) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_200524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "profiles", "users"
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,30 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './config/initializers/simple_form_tailwind.rb'
   ],
-     daisyui: {
-      themes: [
-        {
-          mytheme: {
-            "primary": "#2dd4bf",
-            "secondary": "#d946ef",
-            "accent": "#fbbf24",
-            "neutral": "#3D4451",
-            "base-100": "#FFFFFF",
-            "info": "#3ABFF8",
-            "success": "#34d399",
-            "warning": "#FBBD23",
-            "error": "#ef4444",
-          },
-        },
-      ],
+  theme: {
+    container: {
+      center: true,
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2dd4bf",
+          "secondary": "#d946ef",
+          "accent": "#fbbf24",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#34d399",
+          "warning": "#FBBD23",
+          "error": "#ef4444",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")]
 }
